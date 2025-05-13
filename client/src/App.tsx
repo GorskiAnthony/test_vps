d6c4fc0ade32e61c002eb8572e9c419941ec8f71
Anthony Gorski <38192276+GorskiAnthony@users.noreply.github.com>
2025-05-13 11:09:26 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <header>
-        <h1 className="logo">test vps</h1>
+        <h1 className="logo">qlkdjcsqkl,dcksl<wcjkl<;cw,k<lwcj,kl<w; x<wlkx,clmk<wx,c</h1>
       </header>
 
       <nav className="navbar">
